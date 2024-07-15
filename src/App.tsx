1545d0e5b0af67dd05c0229b6d6982e3091aa6eb
miguelbacalhau <miguelnjacinto@gmail.com>
2024-07-15 18:17:17 +0200

    [feature] simple external store implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { createStore } from './store/store';
+import { useSyncExternalStore } from 'react';
+
+const store = createStore({ resolver: () => ({ count: 1 }) });
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { count } = useSyncExternalStore(store.subscribe, store.getSnapshot);
 
   return (
     <>
@@ -18,7 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => store.setState({ count: count + 1 })}>
           count is {count}
         </button>
         <p>
